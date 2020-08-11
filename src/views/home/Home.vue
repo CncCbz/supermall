42467496c7a51a35eb548a79688a39b4387ecb91
CBZ <1179478342@qq.com>
2020-08-11 09:38:33 +0800

    navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar';
+  import { getHomeMultidata } from 'network/home';
+
   export default {
     name: 'Home',
     data() {
-      return {};
+      return {
+        banner: null,
+        dKeyword: null,
+        keywords: null,
+        recommend: null
+      };
+    },
+    components: {
+      NavBar
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banner = res.data.banner;
+        this.dKeyword = res.data.banner;
+        this.keywords = res.data.keywords;
+        this.recommend = res.data.recommend;
+      });
     }
   };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
